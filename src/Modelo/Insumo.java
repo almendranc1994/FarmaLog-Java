438f7226be7bc10809feae60d4416f120a077eba
almendranc1994 <almendra.nunezc@pucp.pe>
2017-09-30 15:23:15 -0500

    Clase Producto creada

---
+++ src/Modelo/Insumo.java
@@ -0,0 +1,24 @@
+
+package Modelo;
+
+public class Insumo {
+    private int codigoInsumo;
+    private String nombreInsumo;
+        
+    public int getCodigoInsumo() {
+        return codigoInsumo;
+    }
+
+    public void setCodigoInsumo(int codigoInsumo) {
+        this.codigoInsumo = codigoInsumo;
+    }
+
+    public String getNombreInsumo() {
+        return nombreInsumo;
+    }
+
+    public void setNombreInsumo(String nombreInsumo) {
+        this.nombreInsumo = nombreInsumo;
+    }
+    
+}
