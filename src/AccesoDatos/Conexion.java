f93591cdd0a51c143158a1459b50848704081c82
napsta32 <fpenac@pucp.pe>
2017-10-28 19:24:26 +0200

    Verify if connection is closed in every query

--- src/AccesoDatos/Conexion.java
+++ src/AccesoDatos/Conexion.java
@@ -8,6 +8,8 @@ package AccesoDatos;
 import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -27,6 +29,11 @@ public class Conexion {
     private Conexion() {}
     
     public static Connection getConexion() {
+        try {
+            if(conn.isClosed()) conn = null;
+        } catch (SQLException ex) {
+            conn = null;
+        }
         if (conn == null) {
             synchronized(Conexion.class) {
                 if (conn == null) {
