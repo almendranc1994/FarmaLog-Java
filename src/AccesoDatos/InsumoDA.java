42433c88ebee0f28c5431411dae81a7598116453
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2017-11-05 16:24:51 -0500

    Agregado el boton añadir para que abrá la ventana e añadir insumo.

--- src/AccesoDatos/InsumoDA.java
+++ src/AccesoDatos/InsumoDA.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package AccesoDatos;
-
+import java.util.ArrayList;
+import Modelo.Insumo;
+import java.sql.*;
 /**
  *
  * @author HP
  */
 public class InsumoDA {
+    private String ruta="test/clientes.txt";
+    
+    public ArrayList<Insumo> devolverLista(){
+        ArrayList<Insumo> insumos = new ArrayList<Insumo>();
+        try {//registrar el Driver
+            Class.forName("com.mysql.jdbc.Driver");
+            //establecer la conexión
+            Connection con = DriverManager.getConnection("jdbc:mysql://50.62.209.188/20141056", "kpedraza", "Mjw1h88");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return insumos;
+    }
     
 }
