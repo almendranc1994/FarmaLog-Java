556171af840350eb0b421da0d98ccc9486e6ff27
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2017-11-05 16:25:40 -0500

    Agregado el boton añadir para que abrá la ventana e añadir insumo.

--- src/Controlador/InsumoBL.java
+++ src/Controlador/InsumoBL.java
@@ -4,11 +4,21 @@
  * and open the template in the editor.
  */
 package Controlador;
-
+import Modelo.Insumo;
+import AccesoDatos.InsumoDA;
+import java.util.ArrayList;
 /**
  *
  * @author Andre Pando
  */
 public class InsumoBL {
+    private InsumoDA accesoDatos;
     
+    public InsumoBL(){
+        accesoDatos = new InsumoDA();
+    }
+    
+    public ArrayList<Insumo> devolverLista(){
+        return accesoDatos.devolverLista();
+    }
 }
