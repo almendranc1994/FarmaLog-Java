26a74a45765de403ccedceecbdced02955ba45d0
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2017-11-05 22:25:22 -0500

    CreadoProveedorxInsumoBL

---
+++ src/Controlador/ProveedorxInsumoBL.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+import AccesoDatos.InsumoDA;
+import Modelo.ProveedorxInsumo;
+import AccesoDatos.ProveedorxInsumoDA;
+import Modelo.Insumo;
+import java.util.ArrayList;
+/**
+ *
+ * @author Karla Isabel Pedraza Salinas 20141056
+ */
+public class ProveedorxInsumoBL {
+    private InsumoDA accesoDatos;
+    
+    public ProveedorxInsumoBL(){
+        accesoDatos = new ProveedorxInsumoDA();
+    }
+    
+    public ArrayList<ProveedorxInsumo> devolverListaInsumo(String nombre){
+        return accesoDatos.devolverListaInsumo(nombre);
+    }
+}
