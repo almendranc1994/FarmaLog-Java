617a07fd328e9a1b76e6c4138f2b160d5262cdce
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2017-11-05 22:59:00 -0500

    Agregada la clase Marca

---
+++ src/Modelo/Marca.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Karla Isabel Pedraza Salinas 20141056
+ */
+public class Marca {
+    private int idMarca;
+    private String nombre;
+    public Marca(int idMarca, String nombre){
+        this.idMarca=idMarca;
+        this.nombre=nombre;
+    }
+    /**
+     * @return the idMarca
+     */
+    public int getIdMarca() {
+        return idMarca;
+    }
+
+    /**
+     * @param idMarca the idMarca to set
+     */
+    public void setIdMarca(int idMarca) {
+        this.idMarca = idMarca;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
