0617e05cd9eecc346c24bc994cbc9588bbe08654
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2017-11-08 18:20:07 -0500

    logueo

--- src/sitemafamalog/logueo.java
+++ src/sitemafamalog/logueo.java
@@ -32,7 +32,11 @@ public class logueo extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         labelCerrar = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        txtUsuario = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        txtUsuario1 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Logueo");
@@ -63,7 +67,7 @@ public class logueo extends javax.swing.JFrame {
         );
 
         getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 0, 308, 425);
+        jPanel1.setBounds(0, 0, 310, 425);
 
         jPanel2.setBackground(new java.awt.Color(0, 155, 200));
 
@@ -95,29 +99,70 @@ public class logueo extends javax.swing.JFrame {
         getContentPane().add(jLabel3);
         jLabel3.setBounds(330, 70, 250, 40);
 
-        txtUsuario.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
-        txtUsuario.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 51, 51)));
-        txtUsuario.setName("txtUsuario"); // NOI18N
-        txtUsuario.addActionListener(new java.awt.event.ActionListener() {
+        jLabel1.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
+        jLabel1.setText("Contraseña:");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(330, 240, 100, 17);
+
+        txtUsuario1.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
+        txtUsuario1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 51, 51)));
+        txtUsuario1.setName("txtUsuario"); // NOI18N
+        txtUsuario1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtUsuarioActionPerformed(evt);
+                txtUsuario1ActionPerformed(evt);
             }
         });
-        getContentPane().add(txtUsuario);
-        txtUsuario.setBounds(330, 190, 250, 30);
+        getContentPane().add(txtUsuario1);
+        txtUsuario1.setBounds(330, 190, 250, 30);
+
+        jLabel4.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
+        jLabel4.setText("Usuario:");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(330, 160, 100, 17);
+
+        jPasswordField1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 51, 51)));
+        jPasswordField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jPasswordField1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jPasswordField1);
+        jPasswordField1.setBounds(330, 270, 250, 30);
+
+        jButton1.setBackground(new java.awt.Color(0, 155, 200));
+        jButton1.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Ingresar");
+        jButton1.setBorder(null);
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(410, 330, 110, 40);
 
         setSize(new java.awt.Dimension(600, 425));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txtUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUsuarioActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtUsuarioActionPerformed
-
     private void labelCerrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelCerrarMouseClicked
         // TODO add your handling code here:
         this.dispose();
     }//GEN-LAST:event_labelCerrarMouseClicked
+
+    private void txtUsuario1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUsuario1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtUsuario1ActionPerformed
+
+    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPasswordField1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -155,11 +200,15 @@ public class logueo extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JLabel labelCerrar;
-    private javax.swing.JTextField txtUsuario;
+    private javax.swing.JTextField txtUsuario1;
     // End of variables declaration//GEN-END:variables
 }
