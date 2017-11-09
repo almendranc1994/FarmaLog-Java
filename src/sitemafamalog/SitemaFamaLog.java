e31bf063a6cb73543bea21f8d24f1fe76131eb85
Andre Hykire <andpando20@gmail.com>
2017-11-08 22:58:22 -0500

    Modificado main 1.1

--- src/sitemafamalog/SitemaFamaLog.java
+++ src/sitemafamalog/SitemaFamaLog.java
@@ -16,8 +16,8 @@ public class SitemaFamaLog {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        RegistroProveedor frmProveedor = new RegistroProveedor();
-        frmProveedor.setVisible(true);
+        Home frmPrincipal = new Home();
+        frmPrincipal.setVisible(true);
     }
     
 }
