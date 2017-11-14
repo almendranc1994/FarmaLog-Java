9d24c2d5a9b0644194b43970742d401101e4eb7c
MMiltonCM <Teo@192.168.1.12>
2017-11-14 16:43:11 -0500

    Clase añadida para el reporte proveedores

---
+++ src/AccesoDatos/ComprasDA.java
@@ -0,0 +1,35 @@
+package AccesoDatos;
+
+import Modelo.Compras;
+import Modelo.Proveedor;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+//import java.util.Date;
+import java.sql.Date;
+/**
+ *
+ * @author Milton
+ */
+public class ComprasDA {
+    public ArrayList<Proveedor> obtenerListaProveedores(Date fechaI,Date fechaF){
+        ArrayList<Proveedor> lista = new ArrayList<Proveedor>();
+        ProveedoresDA datosProveedor = new ProveedoresDA();
+        
+        try{    
+            Statement sentencia=Conexion.getConexion().createStatement();
+            ResultSet rs = sentencia.executeQuery("SELECT * FROM Compra WHERE fechaCompra>"+fechaI+" AND fechaCompra<"+fechaF);
+            while(rs.next()){
+                int idProveedor = Integer.parseInt(rs.getString("idProvedor"));
+                Proveedor p = datosProveedor.BuscarProveedor(idProveedor);
+                lista.add(p);
+            }
+            Conexion.closeConexion();
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return lista;
+    }
+}
